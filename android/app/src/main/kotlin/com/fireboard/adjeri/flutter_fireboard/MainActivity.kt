bc1987cd1b398aaeb814db6d95dc0e7196a8c210
Adjéri SABI <kod047@gmail.com>
2020-09-12 10:36:37 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/fireboard/adjeri/flutter_fireboard/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.fireboard.adjeri.flutter_fireboard
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_fireboard/ui/home.dart';
+
+void main() {
+  runApp(Home());
+}
