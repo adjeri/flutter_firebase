1aa58659ac714e2bd266c160b1f0213b8f00bf02
Adjéri SABI <kod047@gmail.com>
2020-09-13 04:46:21 +0900

    restructuring custom card

--- lib/ui/custom_card.dart
+++ lib/ui/custom_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:intl/intl.dart';
 
 class CustomCard extends StatelessWidget {
   final QuerySnapshot snapshot;
@@ -15,30 +16,43 @@ class CustomCard extends StatelessWidget {
           height: 150,
           child: Card(
             elevation: 3,
-            child: ListTile(
-              title: Text(snapshot.docs[index].get('title')),
-              subtitle: Text(snapshot.docs[index].get('description')),
-              leading: CircleAvatar(
-                radius: 34,
-                  child: Text(snapshot.docs[index].get('title').toString()[0]),
-              ),
+            child: Column(
+              children: [
+                ListTile(
+                  title: Text(snapshot.docs[index].get('title')),
+                  subtitle: Text(snapshot.docs[index].get('description')),
+                  leading: CircleAvatar(
+                    radius: 34,
+                    child:
+                        Text(snapshot.docs[index].get('title').toString()[0]),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text("By ${snapshot.docs[index].get('name')} "),
+                      Text(_getTimestamp()),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
         ),
-        Text(_getTimestamp())
-        // Text((snapshot.docs[index].get('timestamp') == null) ? "N/A" : snapshot.docs[index].get('timestamp').toString())
-        // Text(snapshot.docs[index].get('title'))
       ],
     );
   }
 
   String _getTimestamp() {
-    try{
-      return snapshot.docs[index].get('timestamp').toString();
-    }
-    catch(error) {
+    try {
+      var timeToDate = new DateTime.fromMillisecondsSinceEpoch(
+          snapshot.docs[index].get('timestamp').seconds * 1000);
+      var dateFormatted = new DateFormat("EEEE, MMM d, y").format(timeToDate);
+      return dateFormatted.toString();
+    } catch (error) {
       return "";
     }
-
   }
 }
